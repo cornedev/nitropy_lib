70a0dff3845ec6cc1ce8bb4ffc52e00b960ec32b
cornedev <corneproductions@gmail.com>
2024-09-01 16:39:55 +0200

    Update nitropy_window_functions.py

--- nitropy/nitropy_window_functions.py
+++ nitropy/nitropy_window_functions.py
@@ -1,14 +1,18 @@
+#creating modules
 import pygame
 
+#this module is made with pygame and sets the windows size:
 def setWindowSize(width, height):
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('nitropy game window')
     return screen
 
+#sets the window title
 def setWindowTitle(title):
     pygame.display.set_caption(title)
 
+#sets the screen color
 def setWindowColor(screen, r, g, b):
     screen.fill((r, g, b))
     pygame.display.flip()
